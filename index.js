7a5e04b7910a74a77d7e4dbb5cfd525fc0799af9
Kat Berge <kat.n.berge@gmail.com>
2020-06-29 14:48:02 -0500

    adds variables of grid, squares, scoreDisplay, startBtn, and width

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const grid = document.querySelector(".grid");
+    // creates an array of all the boxes (each one is a div) in the grid
+    let squares = Array.from(document.querySelectorAll(".grid div")); 
+    const scoreDisplay = document.querySelector("#score");
+    const startBtn = document.querySelector("#start-button")
+    const width = 10;
 })
