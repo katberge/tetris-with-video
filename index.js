0fe492faa9f62276bc839722f78066fca8624058
Kat Berge <kat.n.berge@gmail.com>
2020-06-30 13:17:06 -0500

    this fixes #2 (lTetromino rotation 3 is correct now)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const lTetromino = [
         [1, width + 1, width * 2 + 1, 2],
         [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width, width * 2, width * 2 + 1],
+        [1, width + 1, width * 2, width * 2 + 1],
         [width, width * 2, width * 2 + 1, width * 2 + 2]
     ]
 
